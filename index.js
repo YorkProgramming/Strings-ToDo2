85ba64fd1b24b0a4114d92ad95c3af458e995dc5
Matthew York <myorkprogramming@gmail.com>
2023-08-20 19:34:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//1 
+//Reverse
+
+/* Implement reverseString(str) that, given string, 
+returns that string with characters reversed.
+
+Given "creature", return "erutaerc". 
+Tempting as it seems, do not use the built-in reverse()!
+*/
