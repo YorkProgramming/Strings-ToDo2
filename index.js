bd92a18bdc08ad9d84d4ee071429fd53d35656fe
Matthew York <myorkprogramming@gmail.com>
2023-08-20 19:36:14 -0700

    Reverse

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ returns that string with characters reversed.
 Given "creature", return "erutaerc". 
 Tempting as it seems, do not use the built-in reverse()!
 */
+
+function reverseString(str){
+    var newStr = "";
+    
+    for(var i = str.length-1; i >= 0; i--){
+        newStr += str[i];
+    }
+
+    return newStr;
+}
+
+//Example:
+console.log(reverseString("creature"));
+
+//We set var i to the length of the string -1 
+//because the index of the last character is the length of the string -1
+
+//We then loop through the string backwards and add each character to the new string
+//We return the new string
+
+//#############################################################################################################################
