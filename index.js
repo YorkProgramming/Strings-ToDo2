f04c1d2949ee818b815f55b4397febbc176e7820
Matthew York <myorkprogramming@gmail.com>
2023-08-23 19:59:01 -0700

    Remove even length strings

--- index.js
+++ index.js
@@ -28,3 +28,10 @@ console.log(reverseString("creature"));
 //We return the new string
 
 //#############################################################################################################################
+
+//2
+//Remove Even-Length Strings
+
+/* Build a standalone function to remove strings of even lengths from a given array.
+For array containing ["Nope!","Its","Kris","starting","with","K!","(instead","of","Chris","with","C)","."],
+change that same array to ["Nope!","Its","Chris","."].
