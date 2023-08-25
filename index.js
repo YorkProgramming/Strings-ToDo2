393e269c95678656bbdcf4649fa17d8a24044388
Matthew York <myorkprogramming@gmail.com>
2023-08-24 22:28:13 -0700

    Integer to Roman Numerals

--- index.js
+++ index.js
@@ -57,3 +57,12 @@ console.log(removeEvenLengthStrings(["Nope!","Its","Kris","starting","with","K!"
 //We return the array
 
 //#############################################################################################################################
+
+//3
+//Integer to Roman Numerals
+
+/* Given a positive integer that is less than 4000, 
+return a string containing that value in Roman numeral representation. 
+In this representation, I is 1, V is 5, X is 10, L = 50, C = 100, D = 500, 
+and M = 1000. Remember that 4 is IV, 349 is CCCIL and 444 is CDXLIV.
+*/
